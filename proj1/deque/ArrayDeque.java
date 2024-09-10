997f234f9b58f60f1add4fd04f8fd8be2c5b0206
B Li <b.li2333@hotmail.com>
2024-09-10 21:36:32 +0800

    .

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -2,126 +2,163 @@ package deque;
 
 import java.util.Iterator;
 
+import static java.lang.System.arraycopy;
+
 public class ArrayDeque <T> implements Iterable<T>, Deque<T> {
 
-    private T[] qequeFront;
-    private T[] qequePost;
-    private int sizeFront;
-    private int sizePost;
+    private int size;
+    private T[] items;
+    private int frontNext;
+    private int postNext;
 
     public ArrayDeque() {
-        qequeFront = (T[]) new Object[4];
-        qequePost = (T[]) new Object[4];
-        sizeFront = 0; // cnt of actual elements
-        sizePost = 0;
+        size = 0;
+        items = (T[]) new Object[8];
+        frontNext = 8-1;
+        postNext = 0;
     }
 
-    // arrayIdx takes 1 oe 0, 0 indicates qequeFront, i indicates qequePost.
-    private void resize(int arrayIdx, int cpacity) {
-        T[] newArray = (T[]) new Object[cpacity];
-        if (arrayIdx == 0) {
-            System.arraycopy(qequeFront, 0, newArray, 0, sizeFront);
-            qequeFront = newArray;
-        } else if (arrayIdx == 1) {
-            System.arraycopy(qequePost, 0, newArray, 0, sizePost);
-            qequePost = newArray;
+    private void resize(int capacity) {
+        T[] newArray = (T[]) new Object[capacity];
+        // if no addFirst before
+        if(frontNext < size-1){
+            arraycopy(items,frontNext+1, newArray, 0, (size-1)-frontNext);
+            arraycopy(items, 0, newArray, (size-1)-frontNext, frontNext+1);
         }
+        // if addFirst before
+        else if(frontNext == size-1){
+            arraycopy(items, 0, newArray, 0,size);
+        }
+        items = newArray;
+        frontNext = capacity-1;
+        postNext = size;
     }
 
     @Override
     public void addFirst(T x) {
-        if (sizeFront < qequeFront.length) {
-            qequeFront[sizeFront] = x;
-            sizeFront += 1;
-        } else {
-            resize(0, sizeFront * 2);
-            qequeFront[sizeFront] = x;
-            sizeFront += 1;
+        if(size == items.length){
+            resize(size*2);
         }
+        items[frontNext] = x;
+        frontNext -= 1;
+        if(frontNext < 0){
+            frontNext = items.length-1;
+        }
+        size += 1;
     }
 
     @Override
     public void addLast(T x) {
-        if (sizePost < qequePost.length) {
-            qequePost[sizePost] = x;
-            sizePost += 1;
-        } else {
-            resize(1, sizePost * 2);
-            qequePost[sizePost] = x;
-            sizePost += 1;
+        if(size == items.length){
+            resize(size*2);
         }
+        items[postNext] = x;
+        postNext += 1;
+        if(postNext == items.length){
+            postNext = 0;
+        }
+        size += 1;
+    }
+
+    public String toString() {
+        String res = "";
+        if(frontNext == items.length-1){
+            for(int i = 0; i < postNext; i++){
+                res += items[i]+" ";
+            }
+        } else if(frontNext < items.length-1){
+            for(int i = frontNext+1; i < items.length; i++){
+                res += items[i]+" ";
+            }
+            for(int i = 0; i < postNext; i++){
+                res += items[i]+" ";
+            }
+        }
+        return res;
     }
 
     @Override
     public void printDeque() {
-        String res = "";
-        int i = 0;
-        while (i < sizeFront + sizePost) {
-            res += String.valueOf(get(i)) + " ";
-            i += 1;
-        }
-        System.out.println(res + "\n");
+        System.out.println(this);
     }
 
 //    @Override
 //    public boolean isEmpty() {
-//        return sizeFront == 0 && sizePost == 0;
+//        return size == 0;
 //    }
 
     @Override
     public int size() {
-        return sizeFront + sizePost;
+        return size;
     }
 
     @Override
     public T removeFirst() {
-        if (sizeFront == 0) {
+        if(size == 0){
             return null;
         }
-        if (sizeFront <= qequeFront.length / 4) {
-            resize(0, sizeFront * 2);
+        T res;
+        if(frontNext == items.length-1){
+            res = items[0];
+            items[0] = null;
+            frontNext = 0;
         }
-        T res = qequeFront[sizeFront - 1];
-        qequeFront[sizeFront - 1] = null;
-        sizeFront -= 1;
+        else{
+            res = items[frontNext+1];
+            items[frontNext+1] = null;
+            frontNext += 1;
+        }
+        size -= 1;
+        //resize the size down if it's too wasteful
+        if(size <= items.length/4 && size >= 1){
+            resize(items.length/2);
+        }
         return res;
     }
 
     @Override
     public T removeLast() {
-        if (sizePost == 0) {
+        if(size == 0){
             return null;
         }
-        if (sizePost <= qequePost.length / 4) {
-            resize(1, sizePost * 2);
+        T res;
+        if(postNext != 0){
+            res = items[postNext-1];
+            items[postNext-1] = null;
+            postNext -= 1;
         }
-        T res = qequePost[sizePost - 1];
-        qequePost[sizePost - 1] = null;
-        sizePost -= 1;
+        else{
+            postNext = items.length-1;
+            res = items[postNext];
+            items[postNext] = null;
+        }
+        size -= 1;
+        //resize the size down if it's too wasteful
+        if(size <= items.length/4 && size >= 1){
+            resize(items.length/2);
+        }
         return res;
     }
 
     @Override
     public T get(int i) {
-        if (i < sizeFront) {
-            int targetPos = sizeFront - 1 - i;
-            return qequeFront[targetPos];
+        if(i >= size){
+            return null;
         }
-        else if (i < sizeFront + sizePost) {
-            int targetPos = i - sizeFront;
-            return qequePost[targetPos];
+        int targetIdx;
+        if(frontNext == items.length-1){
+            targetIdx = i;
         }
-        return null;
-    }
-
-    public String toString() {
-        String res = "";
-        int i = 0;
-        while (i < sizeFront + sizePost) {
-            res += String.valueOf(get(i)) + " ";
-            i += 1;
+        else{
+            int dev = (items.length-1)-frontNext;
+            if(i <= dev){
+                targetIdx = frontNext+i;
+            }
+            else{
+                targetIdx = i-dev;
+            }
         }
-        return res;
+        return items[targetIdx];
     }
 
     @Override
@@ -168,13 +205,17 @@ public class ArrayDeque <T> implements Iterable<T>, Deque<T> {
         ArrayDeque<Integer> x = new ArrayDeque<>();
         x.addFirst(1);
         x.addFirst(2);
-        x.addLast(4);
         x.addFirst(5);
-        x.removeFirst();
+        x.addFirst(6);
+        x.addFirst(7);
+        x.addFirst(8);
+        x.addFirst(9);
+        x.addFirst(10);
+        x.addFirst(11);
+        x.addLast(11);
         x.removeLast();
-        for (Integer i : x) {
-            System.out.println(i);
-        }
-
+        x.removeLast();
+        x.removeFirst();
+        x.printDeque();
     }
 }
--- proj1/gh2/GuitarString.java
+++ proj1/gh2/GuitarString.java
@@ -1,7 +1,9 @@
 package gh2;
 
 // TODO: uncomment the following import once you're ready to start this portion
+ import deque.ArrayDeque;
  import deque.Deque;
+ import edu.princeton.cs.algs4.StdRandom;
 // TODO: maybe more imports
 
 //Note: This file will not compile until you complete the Deque implementations
@@ -22,6 +24,11 @@ public class GuitarString {
         //       cast the result of this division operation into an int. For
         //       better accuracy, use the Math.round() function before casting.
         //       Your should initially fill your buffer array with zeros.
+        buffer = new ArrayDeque<Double>();
+        int capacity = (int) Math.round(SR/frequency);
+        for (int i = 0; i < capacity; i++) {
+            buffer.addLast(0.0);
+        }
     }
 
 
@@ -35,6 +42,8 @@ public class GuitarString {
         //       other. This does not mean that you need to check that the numbers
         //       are different from each other. It means you should repeatedly call
         //       Math.random() - 0.5 to generate new random numbers for each array index.
+        double r = Math.random() - 0.5;
+
     }
 
     /* Advance the simulation one time step by performing one iteration of
